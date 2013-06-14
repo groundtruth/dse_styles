4955a6d6597057ad3742e32f551382c904821847
Lotta Atzenbeck <lotta.atzenbeck@gmail.com>
2013-06-14 13:50:45 +1000

    fixed script failing if no sld files to delete

--- bin/process.sh
+++ bin/process.sh
@@ -5,7 +5,7 @@ set -e
 GEOSERVER_LIB='C:\Program Files (x86)\OpenGeo\OpenGeo Suite\webapps\geoserver\WEB-INF\lib'
 RSYNC_DESTINATION='root@s1.pozi.com:/var/lib/tomcat6/webapps/vic_styles/sld/'
 
-ls ./sld/*.sld | xargs rm
+find ./sld -name "*.sld" -exec rm {} \;
 
 java -Djava.awt.headless=true \
     -cp "$GEOSERVER_LIB/*" \
