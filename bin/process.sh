12ba1aa8582595fe98f5fe141f283e1fb163a8c5
Lotta Atzenbeck <lotta.atzenbeck@gmail.com>
2013-06-25 12:10:17 +1000

    adjusted rsync destination to basemap server

--- bin/process.sh
+++ bin/process.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+# set -x
 set -e
 
 GEOSERVER_LIB='C:\Program Files (x86)\OpenGeo\OpenGeo Suite\webapps\geoserver\WEB-INF\lib'
-RSYNC_DESTINATION='root@s1.pozi.com:/var/lib/tomcat6/webapps/vic_styles/sld/'
 
 java -Djava.awt.headless=true \
     -cp "$GEOSERVER_LIB/*" \
--- bin/sync.sh
+++ bin/sync.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-RSYNC_DESTINATION='root@s1.pozi.com:/var/lib/tomcat6/webapps/vic_styles/sld/'
+RSYNC_DESTINATION='root@basemap.pozi.com:/var/lib/tomcat6/webapps/vic_styles/sld/'
 
 rsync --recursive --delete sld/ "$RSYNC_DESTINATION"
 
