da634dd7b2318d082f45288bfc938161bc6f1efb
Lotta Atzenbeck <lotta.atzenbeck@gmail.com>
2013-07-04 17:19:33 +1000

    fixed new process script to actually work

--- bin/process.sh
+++ bin/process.sh
@@ -5,24 +5,33 @@ set -e
 
 GEOSERVER_LIB='C:\Program Files (x86)\OpenGeo\OpenGeo Suite\webapps\geoserver\WEB-INF\lib'
 
+echo -n "Checking CSS"
 unprocessed_css=()
 for css in `find ./css -name '*.css'`
 do
   sld=$(echo $css | sed 's/css/sld/g')
   unprocessed_css+=(`find ${css} -newer ${sld}`)
+  echo -n "."
 done
+echo
 
-java -Djava.awt.headless=true \
-  -cp "$GEOSERVER_LIB/*" \
-  org.geoscript.geocss.Converter \
-  ${unprocessed_css[@]}
+if [[ ${unprocessed_css[@]} =~ ^\s*$ ]]
+then
+  echo "Nothing to do."  
+else
+  echo "Processing: ${unprocessed_css[@]}"
 
-echo "Generated raw SLD files"
+  java -Djava.awt.headless=true \
+    -cp "$GEOSERVER_LIB/*" \
+    org.geoscript.geocss.Converter \
+    ${unprocessed_css[@]}
 
-ls ./css/*.sld | xargs ./bin/rewrap.rb
-ls ./css/*.backup | xargs rm
+  echo "Generated raw SLD files"
 
-mv ./css/*.sld ./sld/
+  ls ./css/*.sld | xargs ./bin/rewrap.rb
+  ls ./css/*.backup | xargs rm
 
-./bin/sync.sh
+  mv ./css/*.sld ./sld/
 
+  ./bin/sync.sh
+fi
